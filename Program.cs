416f4f518cf8ee47e44f637b6ef03ea693fa7b11
YogitaDevi <yogita.gry@gmail.com>
2022-12-28 15:29:10 +0530

    [yogita]:Added Employee check Present or Absent

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿
+using EmployeeWagesProgram;
+using static EmployeeWagesProgram.Compute;
+
+Console.WriteLine("Welcome to Employee Wage Computation Program");
+Compute EmpPresentAbsent = new Compute();
+
+EmpPresentAbsent.PresentAbsent();
